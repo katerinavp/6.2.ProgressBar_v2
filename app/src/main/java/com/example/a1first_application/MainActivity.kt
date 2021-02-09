8edc6faa3627f02ef2e8da13f2513d959933e60f
Kate <bl_ev@mail.ru>
2021-02-09 17:56:24 +0300

    MainActivity скорректирован

--- app/src/main/java/com/example/a1first_application/MainActivity.kt
+++ app/src/main/java/com/example/a1first_application/MainActivity.kt
@@ -3,14 +3,11 @@ package com.example.a1first_application
 
 import android.os.Bundle
 import android.view.LayoutInflater
-
 import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
-
-
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.a1first_application.databinding.ActivityMainBinding
 import io.ktor.client.*
@@ -61,18 +58,9 @@ class MainActivity : AppCompatActivity() {
         val response = client.get<List<Post>>(url)
         println("Десериализация + ${response}")
         client.close()
-        setResponseOnMainThread(response)
+        setResponse(response)
         binding.progressBar.isInvisible = true
 
-    }
-
-
-    private suspend fun setResponseOnMainThread(response: List<Post>) {
-        withContext(Main) {
-            setResponse(response)
-            println("Главный поток + $response")
-            binding.progressBar.visibility = ProgressBar.INVISIBLE
-        }
     }
 
     private fun setResponse(response: List<Post>) {
